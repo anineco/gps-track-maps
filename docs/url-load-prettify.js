c38ac19d650bc2ca61d13a3d6fbf9ba5944bcb52
Anineco Nyanta <anineco@nifty.com>
2019-07-07 13:49:16 +0900

    use preffify

---
+++ docs/url-load-prettify.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', function () {
+  const sources = [];
+  Array.prototype.map.call(document.querySelectorAll('.prettyprint'), function (element) {
+    const url = element.getAttribute('data-url');
+    if (!url) { return; }
+    sources.push(new Promise(function (resolve) {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
+      xhr.responseType = 'text';
+      xhr.addEventListener('loadend', function (event) {
+        if (xhr.status === 200) {
+          const text = xhr.response
+            .replace('ApSrHyswQlSkhiwqypJU_HidSkz0pWKYg7mpCYwpR1oja2ai3CHxJOHpa2LB5NNh', '[YOUR API KEY]')
+            .replace('AIzaSyAxcuxvdd1SeIgtubznQ_JC7e08BZrIHkk', '[YOUR API KEY]');
+          element.appendChild(document.createTextNode(text));
+          resolve();
+        }
+      });
+      xhr.send();
+    }));
+  });
+  Promise.all(sources).then(PR.prettyPrint);
+});
