2bc5c03f98594a8f8cd7fb2bd0546b3945715b75
Anineco Nyanta <anineco@nifty.com>
2019-07-08 23:17:13 +0900

    remove DOMContentLoaded event

--- src/omap_ex1.js
+++ src/omap_ex1.js
@@ -29,105 +29,102 @@ location.search.slice(1).split('&').forEach(function (ma) {
   }
 });
 
-window.addEventListener('DOMContentLoaded', function () {
-  const std = new TileLayer({
-    title: '標準',
-    source: new XYZ({
-      attributions: '<a href="https://maps.gsi.go.jp/development/ichiran.html">地理院タイル</a>',
-      url: 'https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png'
-    })
-  });
-  const pale = new TileLayer({
-    visible: false,
-    title: '淡色',
-    source: new XYZ({
-      attributions: '<a href="https://maps.gsi.go.jp/development/ichiran.html">地理院タイル</a>',
-      url: 'https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png'
-    })
-  });
-  const ort = new TileLayer({
-    visible: false,
-    title: '写真',
+const std = new TileLayer({
+  title: '標準',
+  source: new XYZ({
+    attributions: '<a href="https://maps.gsi.go.jp/development/ichiran.html">地理院タイル</a>',
+    url: 'https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png'
+  })
+});
+const pale = new TileLayer({
+  visible: false,
+  title: '淡色',
+  source: new XYZ({
+    attributions: '<a href="https://maps.gsi.go.jp/development/ichiran.html">地理院タイル</a>',
+    url: 'https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png'
+  })
+});
+const ort = new TileLayer({
+  visible: false,
+  title: '写真',
 /*
-    source: new XYZ({
-      attributions: '<a href="https://maps.gsi.go.jp/development/ichiran.html">地理院タイル</a>',
-      url: 'https://cyberjapandata.gsi.go.jp/xyz/ort/{z}/{x}/{y}.jpg'
-    })
+  source: new XYZ({
+    attributions: '<a href="https://maps.gsi.go.jp/development/ichiran.html">地理院タイル</a>',
+    url: 'https://cyberjapandata.gsi.go.jp/xyz/ort/{z}/{x}/{y}.jpg'
+  })
 */
-    source: new BingMaps({
-      key: 'ApSrHyswQlSkhiwqypJU_HidSkz0pWKYg7mpCYwpR1oja2ai3CHxJOHpa2LB5NNh',
-      imagerySet: 'Aerial'
-    })
-  });
-  const otm = new TileLayer({
-    visible: false,
-    title: 'OTM',
-    source: new XYZ({
-      attributions: [
-        '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors, ',
-        '&copy; <a href="https://opentopomap.org/">OpenTopoMap</a> ',
-        '(<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-      ],
-      url: 'https://tile.opentopomap.org/{z}/{x}/{y}.png'
-    })
-  });
-  const track = new VectorLayer({
-    title: 'GPSデータ',
-    source: new VectorSource({
-      url: param.url,
-      format: new KML()
-    })
-  });
-  const view = new View({
-    center: fromLonLat([param.lon, param.lat]),
-    maxZoom: 18,
-    minZoom: 0,
-    zoom: param.zoom
-  });
-  const map = new Map({
-    target: 'map',
-    view: view,
-    controls: defaults().extend([
-      new ScaleLine()
-    ]),
-    layers: [std, pale, ort, otm, track]
-  });
-  const toolbar = new Toolbar(map, {
-    layers: [std, pale, ort, otm],
-    track: track,
-    disableTerms: true,
-    disableClose: true
-  });
-  map.addControl(toolbar);
+  source: new BingMaps({
+    key: 'ApSrHyswQlSkhiwqypJU_HidSkz0pWKYg7mpCYwpR1oja2ai3CHxJOHpa2LB5NNh',
+    imagerySet: 'Aerial'
+  })
+});
+const otm = new TileLayer({
+  visible: false,
+  title: 'OTM',
+  source: new XYZ({
+    attributions: [
+      '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors, ',
+      '&copy; <a href="https://opentopomap.org/">OpenTopoMap</a> ',
+      '(<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    ],
+    url: 'https://tile.opentopomap.org/{z}/{x}/{y}.png'
+  })
+});
+const track = new VectorLayer({
+  title: 'GPSデータ',
+  source: new VectorSource({
+    url: param.url,
+    format: new KML()
+  })
+});
+const view = new View({
+  center: fromLonLat([param.lon, param.lat]),
+  maxZoom: 18,
+  minZoom: 0,
+  zoom: param.zoom
+});
+const map = new Map({
+  target: 'map',
+  view: view,
+  controls: defaults().extend([
+    new ScaleLine()
+  ]),
+  layers: [std, pale, ort, otm, track]
+});
+const toolbar = new Toolbar(map, {
+  layers: [std, pale, ort, otm],
+  track: track,
+  disableClose: true
+});
+map.addControl(toolbar);
 
-  function getHtml(feature) {
-    return '<h2>' + feature.get('name') + '</h2>' + (feature.get('description') || '');
-  }
+function getHtml(feature) {
+  return '<h2>' + feature.get('name') + '</h2>' + (feature.get('description') || '');
+}
 
-  const popup = new Popup(map);
+const popup = new Popup(map);
 
-  map.on('click', function (evt) {
-    const found = map.forEachFeatureAtPixel(
-      evt.pixel,
-      function (feature, layer) {
-        if (feature.getGeometry().getType() !== 'Point') {
-          return false;
-        }
-        popup.setContent(getHtml(feature));
-        return true;
+map.on('click', function (evt) {
+  const found = map.forEachFeatureAtPixel(
+    evt.pixel,
+    function (feature, layer) {
+      if (feature.getGeometry().getType() !== 'Point') {
+        return false;
       }
-    );
-    popup.setPosition(found ? evt.coordinate : undefined);
-  });
+      popup.setContent(getHtml(feature));
+      return true;
+    }
+  );
+  popup.setPosition(found ? evt.coordinate : undefined);
+});
 
-  map.on('pointermove', function (evt) {
-    if (evt.dragging) { return; }
-    const found = map.forEachFeatureAtPixel(
-      map.getEventPixel(evt.originalEvent),
-      function (feature, layer) {
-        return feature.getGeometry().getType() === 'Point';
-      }
-    );
-    map.getTargetElement().style.cursor = found ? 'pointer' : '';
-  });
+map.on('pointermove', function (evt) {
+  if (evt.dragging) { return; }
+  const found = map.forEachFeatureAtPixel(
+    map.getEventPixel(evt.originalEvent),
+    function (feature, layer) {
+      return feature.getGeometry().getType() === 'Point';
+    }
+  );
+  map.getTargetElement().style.cursor = found ? 'pointer' : '';
 });
--- src/omap_ex2.js
+++ src/omap_ex2.js
@@ -33,156 +33,153 @@ location.search.slice(1).split('&').forEach(function (ma) {
   }
 });
 
-window.addEventListener('DOMContentLoaded', function () {
-  const std = new TileLayer({
-    title: '標準',
-    source: new XYZ({
-      attributions: '<a href="https://maps.gsi.go.jp/development/ichiran.html">地理院タイル</a>',
-      url: 'https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png'
-    })
-  });
-  const pale = new TileLayer({
-    visible: false,
-    title: '淡色',
-    source: new XYZ({
-      attributions: '<a href="https://maps.gsi.go.jp/development/ichiran.html">地理院タイル</a>',
-      url: 'https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png'
-    })
-  });
-  const ort = new TileLayer({
-    visible: false,
-    title: '写真',
+const std = new TileLayer({
+  title: '標準',
+  source: new XYZ({
+    attributions: '<a href="https://maps.gsi.go.jp/development/ichiran.html">地理院タイル</a>',
+    url: 'https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png'
+  })
+});
+const pale = new TileLayer({
+  visible: false,
+  title: '淡色',
+  source: new XYZ({
+    attributions: '<a href="https://maps.gsi.go.jp/development/ichiran.html">地理院タイル</a>',
+    url: 'https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png'
+  })
+});
+const ort = new TileLayer({
+  visible: false,
+  title: '写真',
 /*
-    source: new XYZ({
-      attributions: '<a href="https://maps.gsi.go.jp/development/ichiran.html">地理院タイル</a>',
-      url: 'https://cyberjapandata.gsi.go.jp/xyz/ort/{z}/{x}/{y}.jpg'
-    })
+  source: new XYZ({
+    attributions: '<a href="https://maps.gsi.go.jp/development/ichiran.html">地理院タイル</a>',
+    url: 'https://cyberjapandata.gsi.go.jp/xyz/ort/{z}/{x}/{y}.jpg'
+  })
 */
-    source: new BingMaps({
-      key: 'ApSrHyswQlSkhiwqypJU_HidSkz0pWKYg7mpCYwpR1oja2ai3CHxJOHpa2LB5NNh',
-      imagerySet: 'Aerial'
-    })
-  });
-  const otm = new TileLayer({
-    visible: false,
-    title: 'OTM',
-    source: new XYZ({
-      attributions: [
-        '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors, ',
-        '&copy; <a href="https://opentopomap.org/">OpenTopoMap</a> ',
-        '(<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-      ],
-      url: 'https://tile.opentopomap.org/{z}/{x}/{y}.png'
-    })
-  });
+  source: new BingMaps({
+    key: 'ApSrHyswQlSkhiwqypJU_HidSkz0pWKYg7mpCYwpR1oja2ai3CHxJOHpa2LB5NNh',
+    imagerySet: 'Aerial'
+  })
+});
+const otm = new TileLayer({
+  visible: false,
+  title: 'OTM',
+  source: new XYZ({
+    attributions: [
+      '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors, ',
+      '&copy; <a href="https://opentopomap.org/">OpenTopoMap</a> ',
+      '(<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    ],
+    url: 'https://tile.opentopomap.org/{z}/{x}/{y}.png'
+  })
+});
 
-  const fill = new Fill({ color: 'blue' });
-  const stroke = new Stroke({ color: 'white', width: 2 });
+const fill = new Fill({ color: 'blue' });
+const stroke = new Stroke({ color: 'white', width: 2 });
 
-  function styleFunction(feature) {
-    let style;
-    const type = feature.getGeometry().getType();
-    if (type === 'LineString') {
-      const color = feature.get('_color').match(/^#(..)(..)(..)$/).slice(1).map(h => parseInt(h, 16));
-      color[3] = feature.get('_opacity');
-      style = {
-        stroke: new Stroke({
-          color: color,
-          width: feature.get('_weight'),
-          lineDash: feature.get('_dashArray').split(',')
-        })
-      };
-    } else if (type === 'Point') {
-      style = {
-        image: new Icon({
-          src: feature.get('_iconUrl'),
-          size: feature.get('_iconSize'),
-          anchor: feature.get('_iconAnchor'),
-          anchorXUnits: 'pixels',
-          anchorYUnits: 'pixels',
-          crossOrigin: 'Anonymous'
-        }),
-        text: new Text({
-          text: feature.get('name'),
-          font: 'bold 12px sans-serif',
-          fill: fill,
-          stroke: stroke,
-          textAlign: 'left',
-          offsetX: 10,
-          offsetY: -10
-        })
-      };
-    }
-    return new Style(style);
-  };
-
-  const track = new VectorLayer({
-    title: 'GPSデータ',
-    source: new VectorSource({
-      url: param.url,
-      format: new GeoJSON()
-    }),
-    style: styleFunction
-  });
-  const view = new View({
-    center: fromLonLat([param.lon, param.lat]),
-    maxZoom: 18,
-    minZoom: 0,
-    zoom: param.zoom
-  });
-  const map = new Map({
-    target: 'map',
-    view: view,
-    controls: defaults().extend([
-      new ScaleLine()
-    ]),
-    layers: [std, pale, ort, otm, track]
-  });
-  const toolbar = new Toolbar(map, {
-    layers: [std, pale, ort, otm],
-    track: track,
-    disableTerms: true,
-    disableClose: true
-  });
-  map.addControl(toolbar);
-
-  function getHtml(feature) {
-    let html = '<h2>' + feature.get('name') + '</h2>';
-    const keys = feature.getKeys().filter(
-      key => key !== 'geometry' && key !== 'name' && key.charAt(0) !== '_'
-    );
-    if (keys.length > 0) {
-      html += '<table><tbody><tr><td>'
-           + keys.map(key => key + '</td><td>' + feature.get(key)).join('</td></tr><tr><td>')
-           + '</td></tr></tbody></table>';
-    }
-    return html;
+function styleFunction(feature) {
+  let style;
+  const type = feature.getGeometry().getType();
+  if (type === 'LineString') {
+    const color = feature.get('_color').match(/^#(..)(..)(..)$/).slice(1).map(h => parseInt(h, 16));
+    color[3] = feature.get('_opacity');
+    style = {
+      stroke: new Stroke({
+        color: color,
+        width: feature.get('_weight'),
+        lineDash: feature.get('_dashArray').split(',')
+      })
+    };
+  } else if (type === 'Point') {
+    style = {
+      image: new Icon({
+        src: feature.get('_iconUrl'),
+        size: feature.get('_iconSize'),
+        anchor: feature.get('_iconAnchor'),
+        anchorXUnits: 'pixels',
+        anchorYUnits: 'pixels',
+        crossOrigin: 'Anonymous'
+      }),
+      text: new Text({
+        text: feature.get('name'),
+        font: 'bold 12px sans-serif',
+        fill: fill,
+        stroke: stroke,
+        textAlign: 'left',
+        offsetX: 10,
+        offsetY: -10
+      })
+    };
   }
+  return new Style(style);
+};
 
-  const popup = new Popup(map);
+const track = new VectorLayer({
+  title: 'GPSデータ',
+  source: new VectorSource({
+    url: param.url,
+    format: new GeoJSON()
+  }),
+  style: styleFunction
+});
+const view = new View({
+  center: fromLonLat([param.lon, param.lat]),
+  maxZoom: 18,
+  minZoom: 0,
+  zoom: param.zoom
+});
+const map = new Map({
+  target: 'map',
+  view: view,
+  controls: defaults().extend([
+    new ScaleLine()
+  ]),
+  layers: [std, pale, ort, otm, track]
+});
+const toolbar = new Toolbar(map, {
+  layers: [std, pale, ort, otm],
+  track: track,
+  disableClose: true
+});
+map.addControl(toolbar);
 
-  map.on('click', function (evt) {
-    const found = map.forEachFeatureAtPixel(
-      evt.pixel,
-      function (feature, layer) {
-        if (feature.getGeometry().getType() !== 'Point') {
-          return false;
-        }
-        popup.setContent(getHtml(feature));
-        return true;
+function getHtml(feature) {
+  let html = '<h2>' + feature.get('name') + '</h2>';
+  const keys = feature.getKeys().filter(
+    key => key !== 'geometry' && key !== 'name' && key.charAt(0) !== '_'
+  );
+  if (keys.length > 0) {
+    html += '<table><tbody><tr><td>'
+         + keys.map(key => key + '</td><td>' + feature.get(key)).join('</td></tr><tr><td>')
+         + '</td></tr></tbody></table>';
+  }
+  return html;
+}
+
+const popup = new Popup(map);
+
+map.on('click', function (evt) {
+  const found = map.forEachFeatureAtPixel(
+    evt.pixel,
+    function (feature, layer) {
+      if (feature.getGeometry().getType() !== 'Point') {
+        return false;
       }
-    );
-    popup.setPosition(found ? evt.coordinate : undefined);
-  });
+      popup.setContent(getHtml(feature));
+      return true;
+    }
+  );
+  popup.setPosition(found ? evt.coordinate : undefined);
+});
 
-  map.on('pointermove', function (evt) {
-    if (evt.dragging) { return; }
-    const found = map.forEachFeatureAtPixel(
-      map.getEventPixel(evt.originalEvent),
-      function (feature, layer) {
-        return feature.getGeometry().getType() === 'Point';
-      }
-    );
-    map.getTargetElement().style.cursor = found ? 'pointer' : '';
-  });
+map.on('pointermove', function (evt) {
+  if (evt.dragging) { return; }
+  const found = map.forEachFeatureAtPixel(
+    map.getEventPixel(evt.originalEvent),
+    function (feature, layer) {
+      return feature.getGeometry().getType() === 'Point';
+    }
+  );
+  map.getTargetElement().style.cursor = found ? 'pointer' : '';
 });
