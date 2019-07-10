7ea80065e784e1c3961bed13638fd2516b791d4c
Anineco Nyanta <anineco@nifty.com>
2019-07-11 00:32:54 +0900

    use fetch

--- docs/url-load-prettify.js
+++ docs/url-load-prettify.js
@@ -3,20 +3,15 @@ Array.prototype.map.call(document.querySelectorAll('.prettyprint'), function (el
   const url = element.getAttribute('data-url');
   if (!url) { return; }
   sources.push(new Promise(function (resolve) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.responseType = 'text';
-    xhr.addEventListener('loadend', function (event) {
-      if (xhr.status === 200) {
-        const text = xhr.response
-          .replace('ApSrHyswQlSkhiwqypJU_HidSkz0pWKYg7mpCYwpR1oja2ai3CHxJOHpa2LB5NNh', '[YOUR API KEY]')
-          .replace('AIzaSyAxcuxvdd1SeIgtubznQ_JC7e08BZrIHkk', '[YOUR API KEY]')
-          .replace('dj00aiZpPWR0cjBWa012YVBQNiZzPWNvbnN1bWVyc2VjcmV0Jng9ZDU-', '[YOUR API KEY]');
-        element.appendChild(document.createTextNode(text));
-        resolve();
-      }
+    fetch(url).then(function (response) {
+      return response.text();
+    }).then(function (text) {
+      text = text.replace('ApSrHyswQlSkhiwqypJU_HidSkz0pWKYg7mpCYwpR1oja2ai3CHxJOHpa2LB5NNh', '[YOUR API KEY]')
+                 .replace('AIzaSyAxcuxvdd1SeIgtubznQ_JC7e08BZrIHkk', '[YOUR API KEY]')
+                 .replace('dj00aiZpPWR0cjBWa012YVBQNiZzPWNvbnN1bWVyc2VjcmV0Jng9ZDU-', '[YOUR API KEY]');
+      element.appendChild(document.createTextNode(text));
+      resolve();
     });
-    xhr.send();
   }));
 });
 Promise.all(sources).then(PR.prettyPrint);
