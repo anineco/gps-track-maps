5e9048c1f41c8c79bc65fcce53047dbb71b6fa71
Anineco Nyanta <anineco@nifty.com>
2019-07-14 11:47:10 +0900

    add more explanation

--- docs/url-load-prettify.js
+++ docs/url-load-prettify.js
@@ -7,7 +7,7 @@ Array.prototype.map.call(document.querySelectorAll('.prettyprint'), function (el
       return response.text();
     }).then(function (text) {
       text = text.replace('ApSrHyswQlSkhiwqypJU_HidSkz0pWKYg7mpCYwpR1oja2ai3CHxJOHpa2LB5NNh', '[YOUR API KEY]')
-                 .replace('AIzaSyAxcuxvdd1SeIgtubznQ_JC7e08BZrIHkk', '[YOUR API KEY]')
+                 .replace('AIzaSyDcTd0uhyG2GKcr-IYNkdASbInvr2KyY_Y', '[YOUR API KEY]')
                  .replace('dj00aiZpPWR0cjBWa012YVBQNiZzPWNvbnN1bWVyc2VjcmV0Jng9ZDU-', '[YOUR API KEY]');
       element.appendChild(document.createTextNode(text));
       resolve();
