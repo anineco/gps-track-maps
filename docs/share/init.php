6fcb17fb195e023f127063e321b6c99447dcf81a
Anineco Nyanta <anineco@nifty.com>
2019-12-29 10:45:11 +0900

    add shared resources

---
+++ docs/share/init.php
@@ -0,0 +1,13 @@
+<?php
+function set_init() {
+  return array(
+    'database' => 'nurude_tozan',
+    'host'     => 'localhost',
+//  'host'     => 'mysql720.db.sakura.ne.jp',
+//  'host'     => 'ds119j.local',
+//  'host'     => 'ds120j.local',
+    'user'     => 'nurude',
+    'password' => 'vGEDKhpRjfvXps4J',
+    'socket'   => '/opt/local/var/run/mysql8/mysqld.sock'
+  );
+}
